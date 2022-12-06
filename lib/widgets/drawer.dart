794ff8c700eaa18450e7acf35d8464042f168239
Kawsar Ahmed Shakil <kashakil75@gmail.com>
2022-12-06 18:02:46 +0600

    drawer completed

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -5,6 +5,66 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final imageUrl = 'images/login.png';
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text('Kawsar Ahmed Shakil'),
+                accountEmail: Text('kashakil75@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage(imageUrl),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Home',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.account_circle,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Profile',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Email me',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
